8309d6ee6a6e1b7e7c190beb0403ebbcd327ee1c
Zeng Teng <71592105+zengtengchn@users.noreply.github.com>
2022-11-06 21:02:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,15 +11,7 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Latest package
-# git clone https://github.com/kenzok8/small-package.git package/openwrt-packages
-# Previous package
-# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-# Passwall dependency
-# git clone https://github.com/kenzok8/small.git package/small
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
