d71368464f5b2b8ab9ce547131b0ea63fa1fd510
Zeng Teng <71592105+zengtengchn@users.noreply.github.com>
2023-01-02 19:44:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.1.1/g' package/base-files/files/bin/config_generate
 
 #添加额外软件包
 #svn co clone https://github.com/immortalwrt/luci-app-unblockneteasemusic/trunk package/luci-app-unblockneteasemusic
