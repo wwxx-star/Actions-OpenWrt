4b8a67552ede929d72ed45cf3961a9338408e2c1
wwxx-star <58853590+wwxx-star@users.noreply.github.com>
2023-12-05 18:42:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,16 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.7.1/g' package/base-files/files/bin/config_generate
 
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
+# #git clone -b master  https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+# #git clone https://github.com/Leslie-Wong/luci-app-passwall.git package/passwall/luci/luci-app-passwall
+# #git clone https://github.com/Yangself666/luci-app-passwall.git package/passwall/luci
+# # git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git  package/luci-app-adguardhome 
+# #git clone https://github.com/XiaoBinin/luci-app-adguardhome  package/luci-app-adguardhome 
+# #git clone  https://github.com/Zane-E/luci-app-adguardhome.git  package/luci-app-adguardhome
+# #git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+# #git clone https://github.com/wwxx-star/openwrt-packages1  package/openwrt-packages
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
